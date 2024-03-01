673fbaf710691302a1158f30ddfa83effce25e42
erytan <ntan1695123@gmail.com>
2024-03-01 19:54:06 +0700

    "CRUD coupn"

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const productRouter = require('./product')
 const productCategoryRouter = require('./productCategory')
 const blogCategory = require('./blogCategory')
 const blog = require('./blog')
-
-
+const brand = require('./brand')
+const coupon = require('./coupon')
 
 const initRoutes = (app) => {
     app.use('/api/user', userRouter)
@@ -13,7 +13,8 @@ const initRoutes = (app) => {
     app.use('/api/prodcategory', productCategoryRouter)
     app.use('/api/blogcategory', blogCategory)
     app.use('/api/blog', blog)
-
+    app.use('/api/brand', brand)
+    app.use('/api/coupon', coupon)
 
     app.use(notFound)
     app.use(errHandler)
