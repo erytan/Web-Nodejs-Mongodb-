c6b41fbc2d0e5133475f3569fcf4f28826abe04c
Thoai <Trandamthoai2002@gmail.com>
2024-03-19 22:43:11 +0700

    giao dien login

--- client/src/pages/public/Login.js
+++ client/src/pages/public/Login.js
@@ -1,41 +1,47 @@
 import React, { useState, useCallback } from "react";
-import { InputField, Button } from "../../components";
+import { Input, Button } from "antd";
 import { apiLogin, apiRegister } from "../../apis/user";
-import Swal from "sweetalert2"
+import Swal from "sweetalert2";
+import "antd/dist/reset.css";
+
+const { Password } = Input;
+
 const Login = () => {
   const [payload, setPayload] = useState({
     email: "",
     password: "",
+    firstname: "",
+    lastname: "",
+    phone: "",
+  });
+
+  const resetPayload = () => {
+    setPayload({
+      email: "",
+      password: "",
       firstname: "",
       lastname: "",
-        phone:"",
-  });
-    const resetPayload = () => {
-        setPayload({
-            email: "",
-            password: "",
-              firstname: "",
-              lastname: "",
-                phone:"",
-        })
-    }
-  const [isRegister, setIsRegister] = useState(false);
-  const handleSubmit = useCallback(async () => {
-    const { firstname, lastname,phone, ...data } = payload;
-    if (isRegister) {
-        const response = await apiRegister(payload)
-        if (response.success) {
-            Swal.fire('Congratulation', response.me, 'Success').then(() => {
-                setIsRegister(false)
-                resetPayload()
-            })
-        }
-    } else {
-        const rs = await apiLogin(data)
-        console.log(rs);
-    }
-}, [payload,isRegister]);
+      phone: "",
+    });
+  };
 
+  const [isRegister, setIsRegister] = useState(false);
+
+  const handleSubmit = useCallback(async () => {
+    const { firstname, lastname, phone, ...data } = payload;
+    if (isRegister) {
+      const response = await apiRegister(payload);
+      if (response.success) {
+        Swal.fire("Congratulation", response.me, "Success").then(() => {
+          setIsRegister(false);
+          resetPayload();
+        });
+      }
+    } else {
+      const rs = await apiLogin(data);
+      console.log(rs);
+    }
+  }, [payload, isRegister]);
 
   const handleCreateAccountClick = () => {
     setIsRegister(true);
@@ -47,68 +53,71 @@ const Login = () => {
         <h2>{isRegister ? "Register" : "Login"}</h2>
       </div>
       <form>
-              <div style={{ marginBottom: "15px" }}>
-                  {isRegister && (
-            <InputField
+        {isRegister && (
+          <div style={{ marginBottom: "15px" }}>
+            <Input
               value={payload.firstname}
-              setValue={setPayload}
-              nameKey="firstname"
-              placeholder="firstname"
+              onChange={(e) =>
+                setPayload({ ...payload, firstname: e.target.value })
+              }
+              placeholder="First Name"
             />
-          )}
-              </div>
-              <div style={{ marginBottom: "15px" }}>
-                  {isRegister && (
-            <InputField
-              value={payload.lastname}
-              setValue={setPayload}
-              nameKey="lastname"
-              placeholder="lastname"
-            />
-          )}
-              </div>
-              <div style={{ marginBottom: "15px" }}>
-                  {isRegister && (
-            <InputField
-              value={payload.phone}
-              setValue={setPayload}
-              nameKey="phone"
-              placeholder="phone"
-            />
-          )}
           </div>
-          
+        )}
+        {isRegister && (
+          <div style={{ marginBottom: "15px" }}>
+            <Input
+              value={payload.lastname}
+              onChange={(e) =>
+                setPayload({ ...payload, lastname: e.target.value })
+              }
+              placeholder="Last Name"
+            />
+          </div>
+        )}
+        {isRegister && (
+          <div style={{ marginBottom: "15px" }}>
+            <Input
+              value={payload.phone}
+              onChange={(e) =>
+                setPayload({ ...payload, phone: e.target.value })
+              }
+              placeholder="Phone"
+            />
+          </div>
+        )}
         <div style={{ marginBottom: "15px" }}>
-          
-          <InputField
+          <Input
             value={payload.email}
-            setValue={setPayload}
-            nameKey="email"
+            onChange={(e) =>
+              setPayload({ ...payload, email: e.target.value })
+            }
             placeholder="Email"
           />
         </div>
         <div style={{ marginBottom: "15px" }}>
-          <InputField
+          <Password
             value={payload.password}
-            setValue={setPayload}
-            nameKey="password"
-            type="password"
-            placeholder="Mật khẩu"
+            onChange={(e) =>
+              setPayload({ ...payload, password: e.target.value })
+            }
+            placeholder="Password"
           />
         </div>
         <Button
-          name={isRegister ? "Register" : "Login"}
-          handleOnClick={handleSubmit}
+          type="primary"
+          onClick={handleSubmit}
           style={{ width: "100%", marginBottom: "10px" }}
-          fw
-        />
+        >
+          {isRegister ? "Register" : "Login"}
+        </Button>
         {!isRegister && (
           <p style={{ textAlign: "right" }}>
             <span
               className="text-blue-500 hover:underline cursor-pointer"
               onClick={handleCreateAccountClick}
             >
-              Tạo tài khoản mới
+              Create a new account
             </span>
           </p>
         )}
